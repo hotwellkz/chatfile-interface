6e08ee371b568010d950522702843ece7b3d7f2a
hotwellkz <hotwell.kz@gmail.com>
2025-01-09 17:29:53 +0600

    Update and rename constants.js to constants.cjs

---
+++ src/utils/constants.cjs
@@ -0,0 +1,9 @@
+const MAX_RESPONSE_SEGMENTS = 5;
+const MAX_TOKENS = 500;
+const CONTINUE_PROMPT = 'Please continue from where you left off.';
+
+module.exports = {
+  MAX_RESPONSE_SEGMENTS,
+  MAX_TOKENS,
+  CONTINUE_PROMPT
+};
